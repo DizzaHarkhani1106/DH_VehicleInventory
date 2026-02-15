c7d4359a8d1be55bd357a61196bdab1182b2afc0
DizzaHarkhani1106 <dharkhani2375@conestogac.on.ca>
2026-02-15 18:39:47 -0500

    Created DH_VehicleDto for vehicle response output

---
+++ DH_VehicleInventory.Application/DTOs/DH_VehicleDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DH_VehicleInventory.Application.DTOs
+{
+    public class DH_VehicleDto
+    {
+        public int Id { get; set; }
+        public string VehicleCode { get; set; }
+        public int LocationId { get; set; }
+        public string VehicleType { get; set; }
+        public string Status { get; set; }
+    }
+}

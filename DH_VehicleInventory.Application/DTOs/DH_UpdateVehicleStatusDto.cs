4e70e0e2f9048ab3e14d41ee134b083dc8d9614b
DizzaHarkhani1106 <dharkhani2375@conestogac.on.ca>
2026-02-15 18:38:43 -0500

    Created DH_UpdateVehicleStatusDto for status update input

---
+++ DH_VehicleInventory.Application/DTOs/DH_UpdateVehicleStatusDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DH_VehicleInventory.Application.DTOs
+{
+    public class DH_UpdateVehicleStatusDto
+    {
+        public int Status { get; set; }
+    }
+}

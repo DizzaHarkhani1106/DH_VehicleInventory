8ef4ec0f835e2e72a0d2724b7b40af6b15c9df27
DizzaHarkhani1106 <dharkhani2375@conestogac.on.ca>
2026-02-15 18:37:40 -0500

    Created DH_CreateVehicleDto for vehicle creation input

---
+++ DH_VehicleInventory.Application/DTOs/DH_CreateVehicleDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DH_VehicleInventory.Application.DTOs
+{
+    public class DH_CreateVehicleDto
+    {
+        public string VehicleCode { get; set; }
+        public int LocationId { get; set; }
+        public int VehicleType { get; set; }
+    }
+}

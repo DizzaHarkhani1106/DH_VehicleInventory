bb78425fa08fb08394795c905187b25060325639
DizzaHarkhani1106 <dharkhani2375@conestogac.on.ca>
2026-02-16 17:53:46 -0500

    Add DH_VehicleConfiguration with Fluent API table mapping

---
+++ DH_VehicleInventory.Infrastructure/Repositories/DH_VehicleConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using DH_VehicleInventory.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DH_VehicleInventory.Infrastructure.Repositories
+{
+    public class DH_VehicleConfiguration: IEntityTypeConfiguration<Vehicle>
+    {
+        public void Configure(EntityTypeBuilder<Vehicle> builder)
+        {
+            builder.ToTable("DH_Vehicles");
+
+            builder.HasKey(v => v.Id);
+
+            builder.Property(v => v.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(v => v.VehicleCode)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(v => v.LocationId)
+                .IsRequired();
+
+            builder.Property(v => v.VehicleType)
+                .IsRequired()
+                .HasConversion<int>();
+
+            builder.Property(v => v.Status)
+                .IsRequired()
+                .HasConversion<int>();
+
+            builder.HasIndex(v => v.VehicleCode)
+                .HasDatabaseName("IDX_DH_Vehicles_VehicleCode");
+
+            builder.HasIndex(v => v.Status)
+                .HasDatabaseName("IDX_DH_Vehicles_Status");
+        }
+    }
+}
+

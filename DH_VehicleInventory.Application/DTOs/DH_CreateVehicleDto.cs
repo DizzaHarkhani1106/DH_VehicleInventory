c731aae305a3f5413556c386e7dbc243312d5e3e
DizzaHarkhani1106 <dharkhani2375@conestogac.on.ca>
2026-03-17 14:28:34 -0400

    feat: Complete DDD restructuring - fix all compilation errors

--- DH_VehicleInventory.Application/DTOs/DH_CreateVehicleDto.cs
+++ DH_VehicleInventory.Application/DTOs/DH_CreateVehicleDto.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DH_VehicleInventory.Domain.VehicleAggregate.ValueObjects;
 
 namespace DH_VehicleInventory.Application.DTOs
 {
+
     public class DH_CreateVehicleDto
     {
-        public string VehicleCode { get; set; }
-        public int LocationId { get; set; }
-        public int VehicleType { get; set; }
+        public string VehicleCode { get; set; } = string.Empty;
+        public int VehicleTypeId { get; set; }
+        public VehicleType? GetVehicleType()
+        {
+            return VehicleTypeId switch
+            {
+                1 => VehicleType.Sedan,
+                2 => VehicleType.Suv,
+                3 => VehicleType.Truck,
+                4 => VehicleType.Van,
+                _ => null
+            };
+        }
     }
 }
--- DH_VehicleInventory.Application/DTOs/DH_UpdateVehicleStatusDto.cs
+++ DH_VehicleInventory.Application/DTOs/DH_UpdateVehicleStatusDto.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DH_VehicleInventory.Domain.VehicleAggregate.ValueObjects;
 
 namespace DH_VehicleInventory.Application.DTOs
 {
     public class DH_UpdateVehicleStatusDto
     {
-        public int Status { get; set; }
+        public int VehicleId { get; set; }
+        public int StatusId { get; set; }
+        public VehicleStatus? GetVehicleStatus()
+        {
+            return StatusId switch
+            {
+                1 => VehicleStatus.Available,
+                2 => VehicleStatus.Rented,
+                3 => VehicleStatus.Reserved,
+                4 => VehicleStatus.Maintenance,
+                _ => null
+            };
+        }
     }
 }

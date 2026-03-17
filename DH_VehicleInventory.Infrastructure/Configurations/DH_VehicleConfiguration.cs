808730f52c02b47826d5760f54867453719015a5
DizzaHarkhani1106 <dharkhani2375@conestogac.on.ca>
2026-03-17 03:24:21 -0400

    feat: Add VehicleConfiguration for EF Core mapping with owned value objects

--- DH_VehicleInventory.Infrastructure/Configurations/DH_VehicleConfiguration.cs
+++ DH_VehicleInventory.Infrastructure/Configurations/DH_VehicleConfiguration.cs
@@ -1,41 +1,52 @@
-﻿using DH_VehicleInventory.Domain.Entities;
+﻿using DH_VehicleInventory.Domain.VehicleAggregate.Entities;
+using DH_VehicleInventory.Domain.VehicleAggregate.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace DH_VehicleInventory.Infrastructure.Configurations
+namespace DH_VehicleInventory.Infrastructure.Data.Configurations
 {
-    public class DH_VehicleConfiguration : IEntityTypeConfiguration<Vehicle>
+    public class VehicleConfiguration : IEntityTypeConfiguration<Vehicle>
     {
         public void Configure(EntityTypeBuilder<Vehicle> builder)
         {
             builder.ToTable("DH_Vehicles");
-
             builder.HasKey(v => v.Id);
 
-            builder.Property(v => v.Id)
-                .ValueGeneratedOnAdd();
-
             builder.Property(v => v.VehicleCode)
-                .IsRequired()
-                .HasMaxLength(50);
-
-            builder.Property(v => v.LocationId)
+                .HasMaxLength(20)
                 .IsRequired();
 
-            builder.Property(v => v.VehicleType)
-                .IsRequired()
-                .HasConversion<int>();
+            builder.OwnsOne(v => v.VehicleCode, vc =>
+            {
+                vc.Property(c => c.Code)
+                    .HasColumnName("VehicleCode")
+                    .HasMaxLength(20);
+            });
 
-            builder.Property(v => v.Status)
-                .IsRequired()
-                .HasConversion<int>();
+            builder.OwnsOne(v => v.VehicleType, vt =>
+            {
+                vt.Property(t => t.Id)
+                    .HasColumnName("VehicleTypeId");
 
-            builder.HasIndex(v => v.VehicleCode)
-                .HasDatabaseName("IDX_DH_Vehicles_VehicleCode");
+                vt.Property(t => t.Name)
+                    .HasColumnName("VehicleTypeName")
+                    .HasMaxLength(50);
+            });
 
-            builder.HasIndex(v => v.Status)
-                .HasDatabaseName("IDX_DH_Vehicles_Status");
+            builder.OwnsOne(v => v.Status, vs =>
+            {
+                vs.Property(s => s.Id)
+                    .HasColumnName("StatusId");
+
+                vs.Property(s => s.Name)
+                    .HasColumnName("StatusName")
+                    .HasMaxLength(30);
+            });
+
+            builder.HasMany(v => v.Inventories)
+                .WithOne(i => i.Vehicle)
+                .HasForeignKey(i => i.VehicleId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
-

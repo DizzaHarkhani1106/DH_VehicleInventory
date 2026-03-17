506f4b89b4dbad83f9c214935089caea93958aa9
DizzaHarkhani1106 <dharkhani2375@conestogac.on.ca>
2026-03-17 02:52:53 -0400

    feat: Add Inventory child entity for tracking vehicle location and status

---
+++ DH_VehicleInventory.Domain/VehicleAggregate/Entities/Inventory.cs
@@ -0,0 +1,49 @@
+﻿using DH_VehicleInventory.Domain.SeedWork;
+using DH_VehicleInventory.Domain.VehicleAggregate.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DH_VehicleInventory.Domain.VehicleAggregate.Entities
+{
+    public class Inventory : Entity
+    {
+        public int VehicleId { get; private set; }
+        public Vehicle Vehicle { get; private set; }
+
+        public Location Location { get; private set; }
+        public VehicleStatus Status { get; private set; }
+        public DateTime LastUpdated { get; private set; }
+
+        public Inventory() { }
+
+        public Inventory(
+            int vehicleId,
+            Location location,
+            VehicleStatus status)
+        {
+            if (vehicleId <= 0)
+                throw new ArgumentException("Vehicle id must be positive");
+
+            VehicleId = vehicleId;
+            Location = location ?? throw new ArgumentNullException(nameof(location));
+            Status = status ?? throw new ArgumentNullException(nameof(status));
+            LastUpdated = DateTime.UtcNow;
+        }
+
+        public void UpdateStatus(VehicleStatus newStatus)
+        {
+            Status = newStatus ?? throw new ArgumentNullException(nameof(newStatus));
+            LastUpdated = DateTime.UtcNow;
+        }
+
+        public void MoveToLocation(Location newLocation)
+        {
+            Location = newLocation ?? throw new ArgumentNullException(nameof(newLocation));
+            LastUpdated = DateTime.UtcNow;
+        }
+    }
+}
+
